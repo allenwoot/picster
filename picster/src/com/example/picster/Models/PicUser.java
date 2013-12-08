e3ddd616eca256e2a23e6058bb5c4067dd0afbe6
Allen <wua05@ucla.edu>
2013-12-07 16:02:35 -0800

    Added PicUser

---
+++ picster/src/com/example/picster/Models/PicUser.java
@@ -0,0 +1,20 @@
+package com.example.picster.Models;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+public class PicUser {
+	private String id;
+	private HashMap<String, PicUser> friends;
+	private ParseUser parseUser;
+	public static String defaultPassword = "password";
+	
+	public PicUser(String id, HashMap<String, Object> friends, ParseUser parseUser) {
+		this.id = id;
+		this.friends = null;
+		this.parseUser = parseUser;
+	}
+}
