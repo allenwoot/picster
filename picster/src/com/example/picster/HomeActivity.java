46f2fce9a6928a439952a6981591f28f8fd793d1
Brian Lin <brianlin168@gmail.com>
2013-12-07 16:04:19 -0800

    add picture from gallery

--- picster/src/com/example/picster/HomeActivity.java
+++ picster/src/com/example/picster/HomeActivity.java
@@ -1,15 +1,29 @@
 package com.example.picster;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class HomeActivity extends Activity {
-
+	private static final int SELECT_PHOTO = 100;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		setAddPictureButton();
 	}
 
 	@Override
@@ -18,5 +32,40 @@ public class HomeActivity extends Activity {
 		getMenuInflater().inflate(R.menu.home, menu);
 		return true;
 	}
+	
+    private void setAddPictureButton() {
+    	TextView addPictureButton = (TextView) findViewById(R.id.add_picture_button);
+		addPictureButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onAddPictureClicked();
+			}
+		});
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) { 
+    	super.onActivityResult(requestCode, resultCode, data);
+    	Uri selectedImage = data.getData();
+        InputStream imageStream;
+        // Upload to parse
+		try {
+			imageStream = getContentResolver().openInputStream(selectedImage);
+			Bitmap chosenImageBitmap = BitmapFactory.decodeStream(imageStream);	
+			ImageView imageView = (ImageView) findViewById(R.id.chosen_picture);
+			imageView.setImageBitmap(chosenImageBitmap);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    
+    private void onAddPictureClicked() {
+      Intent intent = new Intent();
+      intent.setType("image/*");
+      intent.setAction(Intent.ACTION_GET_CONTENT);
+      startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PHOTO);	
+    }
 
 }
